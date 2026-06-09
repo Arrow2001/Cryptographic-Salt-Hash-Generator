98051d8cde0f08712c2d6eeda4327ab8380fcbe5
Iain <45532247+Arrowerse2001@users.noreply.github.com>
2026-06-09 23:15:24 +0100

    Uploading.

---
+++ Cryptographic Salt & Hash Gen/Cryptographic Salt & Hash Gen/Program.cs
@@ -0,0 +1,8 @@
+﻿/* This was made solely for my discord / raspberry pi bot
+ * I figured it was better to make a standalone program for salt/hashing than having it bloat the code for the bot.*/
+
+using System.Security.Cryptography;
+
+byte[] token = RandomNumberGenerator.GetBytes(32);
+string salt = Convert.ToHexString(token).ToLower();
+Console.WriteLine(salt); 
